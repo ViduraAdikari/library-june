02a0894e6d2316ec0d300c5dfb14d39a1785261f
vidura <vidura.ce@gmail.com>
2024-06-23 13:19:15 +0530

    Create Author, List authors and delete author

--- src/components/authors/AuthorForm.tsx
+++ src/components/authors/AuthorForm.tsx
@@ -1,16 +1,36 @@
-import React, {FC, PropsWithChildren} from 'react';
+import React, {FC, FormEvent, PropsWithChildren, useState} from 'react';
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import {IAuthor} from "../../types/type";
 
 type AuthorFormProps = {
   onCloseClick: () => void
+  onAuthorCreated: (newAuthor: IAuthor) => void
 }
 
 const AuthorForm: FC<AuthorFormProps> = (props: PropsWithChildren<AuthorFormProps>) => {
 
-  const {onCloseClick} = props;
+  const {onCloseClick, onAuthorCreated} = props;
+
+  const [authorName, setAuthorName] = useState<string | null>(null);
+
+  const handleOnNameChange = (value: string) => {
+    setAuthorName(value);
+  }
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    if (!authorName) {
+      alert("Author name cannot be null");
+      return;
+    }
+
+    const author: IAuthor = {name: authorName};
+    onAuthorCreated(author);
+  }
 
   return (
     <Row className="mt-5 author-form">
@@ -22,10 +42,13 @@ const AuthorForm: FC<AuthorFormProps> = (props: PropsWithChildren<AuthorFormProp
           </Col>
 
           <Col xs={{span: 11, offset: 1}} className="mt-3 d-flex flex-column">
-            <Form>
+            <Form onSubmit={handleSubmit}>
               <Form.Group className="mb-3" controlId="authorName">
                 <Form.Label>Name of Author</Form.Label>
-                <Form.Control type="text"/>
+                <Form.Control type="text"
+                              value={authorName ? authorName : ""}
+                              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                                handleOnNameChange(event.target.value)}/>
               </Form.Group>
 
               <div className="text-end">
